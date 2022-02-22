4079d715bc62d589740501634e466f9f43df9101
ntwaliandy <ntwaliandy90@gmail.com>
2022-02-22 14:45:46 +0300

    modifying homepage simply

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -6,8 +6,45 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // ignore: avoid_unnecessary_containers
-    return Container(
-      child: const Text('hetttttt'),
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: SafeArea(
+          child: Column(
+            children: [
+              const SizedBox(
+                height: 170,
+              ),
+              Align(
+                alignment: Alignment.center,
+                child: Container(
+                  color: const Color(0xFF3A5998),
+                  child: const Text(
+                    " Welcome to Seconds Company",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 40,
+                        fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 20),
+              Align(
+                alignment: Alignment.center,
+                child: Container(
+                  color: const Color(0xFF3A5998),
+                  child: const Text(
+                    " This is where It all Started!!!",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
